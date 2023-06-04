55f46e5ab7f459a2ba4808e0dee0480e7fb105fe
kaustubh-CS <kaustubh.rai@contentstack.com>
2023-06-04 12:26:15 +0530

    key add

---
+++ kes.py
@@ -0,0 +1,21 @@
+# dummy_data.py
+
+# This is a dummy file with some sensitive-looking data.
+
+# Dummy AWS credentials
+AWS_ACCESS_KEY_ID = 'AKIAIOSFODNN7EXAMPLE'
+AWS_SECRET_ACCESS_KEY = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+
+# Dummy database credentials
+DB_USERNAME = 'admin'
+DB_PASSWORD = 'supersecretpassword'
+
+# Dummy API key
+API_KEY = '123456789abcdef123456789abcdef12'
+
+# Dummy private RSA key
+PRIVATE_KEY = """
+-----BEGIN RSA PRIVATE KEY-----
+MIICXgIBAAKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cJ7lBqQIDAQABAoGBALesy++GyYODzYBF+XeIYp5t8GZzgSuLJMEnebP8zUZENjJ3LoTfy2YwRe1t0IkwA8+7WYxAtqRZOg9aZvQ7oIfxWpg9VwW9f5/3hUhBW/KDxHcRLvRk3o3w8Bm5M1VHNeXSKqx1x5+L1iTCaJjmu3xjeaJW2CwO2XaYT4ObtZHb
+-----END RSA PRIVATE KEY-----
+"""
